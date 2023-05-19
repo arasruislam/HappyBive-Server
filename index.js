c88375e2ef4728d02e16545da3038744cbe2f8f3
asru-islam <arasru01@gmail.com>
2023-05-19 15:58:27 +0600

    add shopbycategory routes and set mongodb

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /* All required Files */
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -9,6 +10,51 @@ const port = process.env.PORT || 5000;
 /* Middleware's */
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w00ka8a.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    //   Database Collection
+    const categoryCollection = client
+      .db("happyBive")
+      .collection("shopByCategory");
+
+    app.get("/subCategories", async (req, res) => {
+      let query = {};
+
+      if (req.query?.category) {
+        query = {
+          category: req.query.category,
+        };
+      }
+
+      const result = await categoryCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("The Server is Running");
